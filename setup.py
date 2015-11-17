c5b1163ccf3e99efda7ecf542b9d6ee92927b5cc
Bernhard Kuzel <bernhard.kuzel@gmail.com>
2015-11-17 16:00:46 +0100

    no nailed versions, create tag script

--- setup.py
+++ setup.py
@@ -10,35 +10,7 @@ execfile(os.path.join(os.path.dirname(__file__),
                       'lovely/esdb/__init__.py'))
 
 
-def get_versions():
-    """picks the versions from version.cfg and returns them as dict"""
-    versions_cfg = os.path.join(os.path.dirname(__file__), 'versions.cfg')
-    config = ConfigParser.ConfigParser()
-    config.optionxform = str
-    config.readfp(open(versions_cfg))
-    return dict(config.items('versions'))
-
-
-def nailed_requires(requirements, pat=re.compile(r'^(.+)(\[.+\])?$')):
-    """returns the requirements list with nailed versions"""
-    versions = get_versions()
-    res = []
-    for req in requirements:
-        if '[' in req:
-            name = req.split('[', 1)[0]
-        else:
-            name = req
-        if name in versions:
-            res.append('%s==%s' % (req, versions[name]))
-        else:
-            res.append(req)
-    return res
-
-
 requires = [
-    'setuptools',
-    'pyes',
-    'lovely.settings',
     'gevent',
     'elasticsearch',
 ]
@@ -51,11 +23,11 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     extras_require=dict(
-          test=nailed_requires([
+          test=[
               'collective.xmltestreport',
               'crate',
               'lovely.testlayers',
-          ]),
+          ],
     ),
     zip_safe=False,
     install_requires=requires,
