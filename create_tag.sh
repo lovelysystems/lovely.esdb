fee725fad362cd6af6864a555cd05ef63a1fdb4f
Bernhard Kuzel <bernhard.kuzel@gmail.com>
2015-11-17 16:20:05 +0100

    new script for create tag

--- create_tag.sh
+++ create_tag.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # check if everything is committed
-# CLEAN=`git status -s`
-# if [ ! -z "$CLEAN" ]
-# then
-#   echo "Working directory not clean. Please commit all changes before tagging"
-#   echo "Aborting."
-#   exit -1
-# fi
+CLEAN=`git status -s`
+if [ ! -z "$CLEAN" ]
+then
+   echo "Working directory not clean. Please commit all changes before tagging"
+   echo "Aborting."
+   exit -1
+fi
 
 echo "Fetching origin..."
 git fetch origin > /dev/null
 
 # check if current branch is master
 BRANCH=`git branch | grep "^*" | cut -d " " -f 2`
-# if [ "$BRANCH" != "master" ]
-# then
-#    echo "Current branch is $BRANCH. Must be master."
-#    echo "Aborting."
-#    exit -1
-# fi
+if [ "$BRANCH" != "master" ]
+then
+   echo "Current branch is $BRANCH. Must be master."
+   echo "Aborting."
+   exit -1
+fi
 
 # check if master == origin/master
 MASTER_COMMIT=`git show --format="%H" master`
@@ -33,7 +33,7 @@ then
 fi
 
 # check if tag to create has already been created
-VERSION=`grep version setup.py | cut -d "'" -f 2`
+VERSION=`./bin/py setup.py --version`
 EXISTS=`git tag | grep $VERSION`
 
 if [ "$VERSION" == "$EXISTS" ]
@@ -43,11 +43,11 @@ then
    exit -1
 fi
 
-# check if VERSION is in head of CHANGES.txt
-REV_NOTE=`grep "[0-9/]\{10\} $VERSION" CHANGES.txt`
+# check if VERSION is in head of CHANGES.rst
+REV_NOTE=`grep "[0-9/]\{10\} $VERSION" CHANGES.rst`
 if [ -z "$REV_NOTE" ]
 then
-    echo "No notes for revision $VERSION found in CHANGES.txt"
+    echo "No notes for revision $VERSION found in CHANGES.rst"
     echo "Aborting."
     exit -1
 fi
